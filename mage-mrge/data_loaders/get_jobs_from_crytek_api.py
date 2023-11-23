7d52cf2665636ce43cb48b9aca8f2f8298086dc6
extern-scherbach-kiril <kiril.scherbach@gmail.com>
2023-11-23 15:49:02 +0000

    add dbt

--- mage-mrge/data_loaders/get_jobs_from_crytek_api.py
+++ mage-mrge/data_loaders/get_jobs_from_crytek_api.py
@@ -1,4 +1,4 @@
-import io
+from datetime import datetime
 
 import pandas as pd
 import requests
@@ -14,7 +14,9 @@ def load_data_from_api(*args, **kwargs):
     """
     Template for loading data from API
     """
+    logger = kwargs.get("logger")
     i = 0
+    dl = []
     base_url = "https://www.crytek.com/api/v1/lever-postings"
     logger.info(f"Requesting {base_url}")
     r = requests.get(base_url)
@@ -23,7 +25,7 @@ def load_data_from_api(*args, **kwargs):
     position_count = len(jobs)
     for job in jobs:
         url = f"""{base_url}/{job["alias"]}"""
-        logger.info(f"""Requesting {base_url}/{job["alias"]}""")
+        logger.debug(f"""Requesting {base_url}/{job["alias"]}""")
         j = requests.get(url)
         jd = j.json()
         data_dict = {
@@ -42,49 +44,13 @@ def load_data_from_api(*args, **kwargs):
             "date_posted": jd.get("date_posted", "2000-01-01"),
             "valid_through": jd.get("valid_through", "2000-01-01"),
             "full_response": j.text,
+            "insert_ts": datetime.now().isoformat(),
         }
-        logger.info(f"""Inserting requisition_id {jd.get("id", "N/A")}""")
-        query = """
-        INSERT INTO jobs_crytek (
-                        job_id
-                    , additional_plain
-                    , created_at
-                    , description_plain
-                    , lists
-                    , job_title
-                    , hosted_url
-                    , apply_url
-                    , commitment
-                    , department
-                    , job_location
-                    , team
-                    , date_posted
-                    , valid_through
-                    , full_response
-                    , insert_ts)
-        VALUES (
-            %(job_id)s
-        , %(additional_plain)s
-        , %(created_at)s
-        , %(description_plain)s
-        , %(lists)s
-        , %(job_title)s
-        , %(hosted_url)s
-        , %(apply_url)s
-        , %(commitment)s
-        , %(department)s
-        , %(job_location)s
-        , %(team)s
-        , %(date_posted)s
-        , %(valid_through)s
-        , %(full_response)s
-        , NOW()::timestamp
-        )
-        """
-        cur.execute(query, data_dict)
+        logger.debug(f"""Inserting requisition_id {jd.get("id", "N/A")}""")
+        dl.append(data_dict)
         i += 1
 
-    return pd.read_csv(io.StringIO(response.text), sep=",")
+    return pd.DataFrame(dl)
 
 
 @test
@@ -92,4 +58,4 @@ def test_output(output, *args) -> None:
     """
     Template code for testing the output of the block.
     """
-    assert output is not None, "The output is undefined"
+    assert len(output.index) >= 1, "The output has at least 1 row"
---
+++ mage-mrge/dbt/animated-broccoli-dbt/models/jobs_epic_daily.sql
@@ -0,0 +1,52 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key='daily_job_id',
+    description='This table contains a single open position from Epic registered as open per day and can be used for further analysis',
+    alias='jobs_epic_daily',
+    indexes=[
+      {'columns': ['job_id', 'insert_date'], 'type': 'btree', 'unique': True},
+      {'columns': ['insert_ts'], 'type': 'btree'},
+    ]
+  )
+}}
+
+select
+    daily_job_id
+    , absolute_url
+    , title
+    , department
+    , company
+    , remote
+    , updated_at
+    , insert_ts
+    , insert_date
+    , internal_job_id || requisition_id as job_id
+    , city || ', ' || state || ', ' || country as job_location
+from
+    (
+        select
+            absolute_url
+            , internal_job_id
+            , requisition_id
+            , title
+            , department
+            , company
+            , remote
+            , spotlight
+            , city
+            , state
+            , country
+            , updated_at
+            , insert_ts
+            , insert_ts::date as insert_date
+            , concat(internal_job_id, '-', requisition_id, '-', insert_ts::date) as daily_job_id
+            , row_number() over (partition by concat(internal_job_id, '-', requisition_id, '-', insert_ts::date) order by insert_ts desc) as rn
+        from {{ source('scraper_results', 'jobs_epic') }}
+        {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        -- (uses > to include records whose timestamp occurred since the last run of this model)
+            where insert_ts > (select max(insert_ts) from {{ this }})
+        {% endif %}
+    ) as ordered_incr
+where rn = 1
