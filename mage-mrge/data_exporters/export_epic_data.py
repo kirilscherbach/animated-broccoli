b0fa9b3305c128d396ee634a124431506358800e
extern-scherbach-kiril <kiril.scherbach@gmail.com>
2024-01-03 14:57:27 +0100

    job operations

--- mage-mrge/data_exporters/export_epic_data.py
+++ mage-mrge/data_exporters/export_epic_data.py
@@ -18,7 +18,7 @@ def export_data_to_postgres(df: DataFrame, **kwargs) -> None:
     Docs: https://docs.mage.ai/design/data-loading#postgresql
     """
     schema_name = "public"
-    table_name = kwargs["table_name"]
+    table_name = "jobs_epic"
     config_path = path.join(get_repo_path(), "io_config.yaml")
     config_profile = "default"
 
