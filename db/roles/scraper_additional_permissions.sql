9525df1e9371afddbce03c727ebb666378bcd2fd
extern-scherbach-kiril <kiril.scherbach@gmail.com>
2024-01-04 11:07:07 +0100

    Add permission for Chymera daily update

--- db/roles/scraper_additional_permissions.sql
+++ db/roles/scraper_additional_permissions.sql
@@ -2,3 +2,4 @@ grant all on public.jobs_crytek_clean to scraper;
 grant all on public.jobs_epic_clean to scraper;
 grant all on public.jobs_crytek_daily_update to scraper;
 grant all on public.jobs_epic_daily_update to scraper;
+grant all on public.jobs_chymera_daily_update to scraper;
